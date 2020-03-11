f300b8bc9a916e48119779d297e781346a2d8eb2
Ha-sake <57680069+Ha-sake@users.noreply.github.com>
2020-03-11 15:42:58 +0000

    Junção de funções
    Junção das funções que criam os ficheiros de drones (droneFileMaker()) e o ficheiro de timetable (timetableFileMaker()) em apenas uma função fileMaker()

--- writeFilesNovo.py
+++ writeFilesNovo.py
@@ -12,67 +12,72 @@ from copy import deepcopy
 from FileNames import FileNames
 
 
-def droneFileMaker(droneFileName):
-    """
+def FileMaker(FileNameCombo):
     """
 
-    date = r.readHeader(droneFileName).getDate()
-    updatedDate = date
+    """
+
+    droneFileName = FileNameCombo.getDroneFileName()
+    parcelFileName = FileNameCombo.getParcelFileName()
+
+    ################################################# CREATING PARCEL FILE
+
+    droneDate = r.readHeader(droneFileName).getDate()
+    updatedDroneDate = droneDate
     time = r.readHeader(droneFileName).getTime()
-    updatedTime = str(t.updateTime(time, 30))
+    updatedDroneTime = str(t.updateTime(time, 30))
 
-    if t.hourToDatetime(updatedTime)<t.hourToDatetime("08:00"):
-        updatedTime = "08h00"
-        updatedDate = t.updateDate(date, 1)
+    if t.hourToDatetime(updatedDroneTime)<t.hourToDatetime("08:00"):
+        updatedDroneTime = "08h00"
+        updatedDroneDate = t.updateDate(droneDate, 1)
 
-    # breaking down updatedDate into separate day, year and month parts
+    # breaking down dates into separate day, year and month parts
     
     
-    if updatedDate[1]=="-" or updatedDate[2]=="-":
-        updatedDay = updatedDate.split("-")[0]
-        updatedYear = updatedDate.split("-")[2]
-        updatedMonth = updatedDate.split("-")[1]
+    if updatedDroneDate[1]=="-" or updatedDroneDate[2]=="-":
+        updatedDroneDay = updatedDroneDate.split("-")[0]
+        updatedDroneYear = updatedDroneDate.split("-")[2]
+        updatedDroneMonth = updatedDroneDate.split("-")[1]
 
     else:
-        updatedDay = updatedDate.split("-")[2]
-        updatedYear = updatedDate.split("-")[0]
-        updatedMonth = updatedDate.split("-")[1]
+        updatedDroneDay = updatedDroneDate.split("-")[2]
+        updatedDroneYear = updatedDroneDate.split("-")[0]
+        updatedDroneMonth = updatedDroneDate.split("-")[1]
 
     # formatting new file's name
 
-    updatedFileName = "drones{0}h{1}_{2}y{3}m{4}.txt".format(updatedTime[:2], updatedTime[3:5], updatedYear, updatedMonth, updatedDay)
+    updatedDroneFileName = "drones{0}h{1}_{2}y{3}m{4}.txt".format(updatedDroneTime[:2], updatedDroneTime[3:5], updatedDroneYear, updatedDroneMonth, updatedDroneDay)
     
-    updatedFile = open(updatedFileName, "w")
-    updatedFile.close()
-    
-    return updatedFileName
+    updatedDroneFile = open(updatedDroneFileName, "w")
+    updatedDroneFile.close()
 
-def timetableFileMaker(parcelFileName):
-    """
-    """
+    ################################################# CREATING PARCEL FILE
 
-    date = r.readHeader(parcelFileName).getDate()
-    time = r.readHeader(parcelFileName).getTime()
+    parcelDate = r.readHeader(parcelFileName).getDate()
+    parcelTime = r.readHeader(parcelFileName).getTime()
 
     # breaking down date into separate day, year and month parts
     
-    if date[1]=="-" or date[2]=="-":
-        updatedDay = date.split("-")[0]
-        updatedYear = date.split("-")[2]
-        updatedMonth = date.split("-")[1]
+    if parcelDate[1]=="-" or parcelDate[2]=="-":
+        updatedParcelDay = parcelDate.split("-")[0]
+        updatedParcelYear = parcelDate.split("-")[2]
+        updatedParcelMonth = parcelDate.split("-")[1]
 
     else:
-        updatedDay = date.split("-")[2]
-        updatedYear = date.split("-")[0]
-        updatedMonth = date.split("-")[1]
+        updatedParcelDay = parcelDate.split("-")[2]
+        updatedParcelYear = parcelDate.split("-")[0]
+        updatedParcelMonth = parcelDate.split("-")[1]
 
     # formatting new file's name
 
-    updatedFileName = "timetable{0}h{1}_{2}y{3}m{4}.txt".format(time[:2], time[3:5], updatedYear, updatedMonth, updatedDay)
+    updatedParcelFileName = "timetable{0}h{1}_{2}y{3}m{4}.txt".format(parcelTime[:2], parcelTime[3:5], updatedParcelYear, updatedParcelMonth, updatedParcelDay)
     
-    updatedFile = open(updatedFileName, "w")
-    updatedFile.close()
+    updatedParcelFile = open(updatedParcelFileName, "w")
+    updatedParcelFile.close()
     
-    return updatedFileName
+    return FileNames(updatedDroneFileName, updatedParcelFileName)
 
 
+FileNameCombo = FileNames("drones11h00_2019y11m5.txt", "parcels11h00_2019y11m5.txt")
+
+print(FileMaker(FileNameCombo).getDroneFileName(), FileMaker(FileNameCombo).getParcelFileName())
