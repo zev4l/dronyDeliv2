daebbe881c387a1c2c9fff3d31a75cffeb974037
Ha-sake <57680069+Ha-sake@users.noreply.github.com>
2020-03-21 16:27:34 +0000

    Alterei o nome do dronyDNovo para dronyD.py sendo que já estava terminado

--- dronyD.py
+++ dronyD.py
@@ -3,47 +3,32 @@
 # 55373 José Almeida
 # 54975 Miguel Lages
 
-# Main File: being the main file, this is where the functions written in the helper modules and called and were the program's flow is defined.
 
-import constants as c
 import organize as o
 import readFiles as r
-import time_kit as t
 import writeFiles as w
+from FileNames import FileNames
 import sys
-from pprint import pprint
-from copy import deepcopy
 
 if __name__ == "__main__":
     arg1 = str(sys.argv[1])
     arg2 = str(sys.argv[2])
-    fileDict = r.filePointer(arg1, arg2)
+    originalFileNames = FileNames(arg1, arg2)
 
-droneFile = fileDict["droneFile"]
-parcelFile = fileDict["parcelFile"]
-
-
+droneFile = originalFileNames.getDroneFileName()
+parcelFile = originalFileNames.getParcelFileName()
 r.fileValidator(droneFile, parcelFile)
 
+newFileNames = FileNames(w.FileMaker(originalFileNames).getDroneFileName(), w.FileMaker(originalFileNames).getParcelFileName())
+
+w.headerWriter(originalFileNames, newFileNames)
+
+droneList = r.droneLister(originalFileNames.getDroneFileName())
+parcelList = r.parcelLister(originalFileNames.getParcelFileName())
+ComboList = o.droneAssigner(droneList, parcelList)
+
+w.coreTimetableWriter(ComboList, newFileNames)
+w.coreDroneWriter(droneList, newFileNames)
 droneList = r.droneLister(droneFile)
 parcelList = r.parcelLister(parcelFile)
 
-# TimetableFileName = w.timetableFileMaker(fileDict)
-# w.headerWriter("Timetable", fileDict, TimetableFileName)
-# w.timetableWriter(o.droneAssigner(droneList, parcelList), TimetableFileName)
-
-# newDroneFileName = w.droneFileMaker(fileDict)
-# w.headerWriter("Drones", fileDict, newDroneFileName)
-# w.droneWriter(o.droneAssigner(droneList, parcelList), newDroneFileName)
-
-a = o.droneAssigner(droneList, parcelList)
-b = deepcopy(a)
-
-newDroneFileName = w.droneFileMaker(fileDict)
-w.headerWriter("Drones", fileDict, newDroneFileName)
-w.droneWriter(a, newDroneFileName)
-
-
-TimetableFileName = w.timetableFileMaker(fileDict)
-w.headerWriter("Timetable", fileDict, TimetableFileName)
-w.timetableWriter(b, TimetableFileName)
